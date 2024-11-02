1ae5eb07c2c03a9a451dcbc5af09fd81ba465a92
Dinesh <85795537+dinesh-git07@users.noreply.github.com>
2024-11-02 19:44:12 +0530

    updated vercel config

--- main.py
+++ main.py
@@ -37,3 +37,13 @@ async def flatten_query_string_lists(request: Request, call_next):
 def health():
     return {"status": "ok"};
 
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=5001,
+        reload= True,
+    )
+    
